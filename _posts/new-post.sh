4b29dd93eae9f301004afa0336e34624504188bb
qzydustin <44362429+qzydustin@users.noreply.github.com>
2023-08-27 15:17:27 -0700

    Update new post script

--- _posts/new-post.sh
+++ _posts/new-post.sh
@@ -3,26 +3,30 @@
 echo -n "Enter a title for your post and press [ENTER]: "
 read INPUT
 
-if [ -z "$INPUT" ]
-   then
-   echo "Post title is required"
-   exit 1
+if [ -z "$INPUT" ]; then
+    echo "Post title is required"
+    exit 1
 fi
+
+# Remove special characters and replace spaces with hyphens
+CLEAN_TITLE=$(echo "$INPUT" | tr -d '[:punct:]' | tr ' ' '-')
+LOWER=$(echo "$CLEAN_TITLE" | tr '[:upper:]' '[:lower:]')
+
 echo -n "Enter a category for your post and press [ENTER]: "
 read CATEGORY
-if [ -z "$CATEGORY" ]
-   then
-   echo "Post category is required"
-   exit 1
+
+if [ -z "$CATEGORY" ]; then
+    echo "Post category is required"
+    exit 1
 fi
+
 DATE=$(date +"%Y-%m-%d")
 TIME=$(date +"%T")
-STRPTITLE=${INPUT// /-}
-LOWER=$(echo "$STRPTITLE" | tr '[:upper:]' '[:lower:]')
 FILENAME=$(printf "%s-%s.md" "$DATE" "$LOWER")
 
-touch $FILENAME
-cat > $FILENAME << EOL
+touch "$FILENAME"
+
+cat > "$FILENAME" << EOL
 ---
 layout: post
 title:  $INPUT
